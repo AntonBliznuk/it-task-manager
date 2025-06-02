a08310e88e75ce994ef958b7a767e830d84b9452
Антон <antonbliznuk71@gmail.com>
2025-06-02 10:35:51 +0300

    feat: implement home page view, add base and index HTML templates, and update URL patterns

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from main import views
 
 
 urlpatterns = [
+    path("", views.index, name="index"),
 ]
 
 app_name = "main"
--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from main.models import TaskType, Position, Worker, Task
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    context = {
+        "num_of_workers": Worker.objects.all().count(),
+        "num_of_tasks": Task.objects.all().count(),
+        "num_of_positions": Position.objects.all().count()
+    }
+    return render(request, "main/index.html", context=context)
