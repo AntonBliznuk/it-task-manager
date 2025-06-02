907eddf9acbfcc7060bbf148c16148ad6a48e02a
Антон <antonbliznuk71@gmail.com>
2025-06-02 11:06:11 +0300

    feat: Implement CRUD operations for Position model and add basic HTML

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,12 @@ from main import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+
+    path("positions/", views.PositionListView.as_view(), name="position-list"),
+    path("positions/create/", views.PositionCreateView.as_view(), name="position-create"),
+    path("positions/<int:pk>/update/", views.PositionUpdateView.as_view(), name="position-update"),
+    path("positions/<int:pk>/delete/", views.PositionDeleteView.as_view(), name="position-delete"),
+
 ]
 
 app_name = "main"
--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.views import generic
+from django.urls import reverse_lazy
+
 from main.models import TaskType, Position, Worker, Task
 
 
@@ -10,3 +13,25 @@ def index(request: HttpRequest) -> HttpResponse:
         "num_of_positions": Position.objects.all().count()
     }
     return render(request, "main/index.html", context=context)
+
+
+class PositionListView(generic.ListView):
+    model = Position
+
+
+class PositionCreateView(generic.CreateView):
+    model = Position
+    fields = "__all__"
+    success_url = reverse_lazy("main:position-list")
+
+
+class PositionUpdateView(generic.UpdateView):
+    model = Position
+    fields = "__all__"
+    success_url = reverse_lazy("main:position-list")
+
+
+class PositionDeleteView(generic.DeleteView):
+    model = Position
+    template_name = "main/position_confirm_delete.html"
+    success_url = reverse_lazy("main:position-list")
